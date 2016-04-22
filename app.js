7b9009f1f4c65d163bf054bc5beed57bb25c8979
shghdyji <sh.ghdyji@gmail.com>
2016-04-22 17:36:31 +0900

    call

--- app.js
+++ app.js
@@ -29,12 +29,38 @@ app.get('/self', function (req, res) {
 	res.render('self.html',{})
 });
 app.use('/', routes);
+app.get('/call', function (req, res) {
+  var client = require('twilio')( "AC3c08a8aa6a616c4b70dadb3e0ba98175", "e0dd7f2795760d32ec1b859309fe2746");
+  var twilio = require('twilio');
+  var resp = new twilio.TwimlResponse();
+
+  resp.say('Welcome to Twilio!');
+  resp.say('Please let us know if we can help during your development.', {
+      voice:'woman',
+      language:'en-gb'
+  });
+
+  console.log(resp.toString());
+  client.makeCall({
+      to:"+819052330123", // Any number Twilio can call
+      from: "+815031595871", // A number you bought from Twilio and can use for outbound communication
+      url:   "https://demo.twilio.com/welcome/voice/ja/"
+      // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+}, function(err, responseData) {
+  console.log(err);
+  //executed when the call has been initiated.
+  console.log(responseData.from); // outputs "+14506667788"
+
+  });
+
+})
 app.use('/users', users);
 app.use('/message', function (req, res) {
-		res.render('message.html', {});
+  res.render('message.html', {});
 })
 app.use('/order', function (req, res) {
-	res.render('order.html', {});
+  res.render('order.html', {});
 })
 app.use('/detail/dokopro', function (req, res) {
 		res.render('dokopro_detail.html', {});
